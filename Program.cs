1e0ee074353e9ca7a2a540db0f03b42fbf10a892
Pedro Salazar <ph.s.salazar@gmail.com>
2024-01-03 18:52:01 -0300

    Finalização de Testes da Trilha .NET POO da DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste com o Smartphone NOKIA");
+Smartphone nokia = new Nokia(numero: "21999999999", modelo: "MODELO NOKIA", imei: "0000000001", memoria: 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n-----");
+
+Console.WriteLine("Teste com Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "1188888888", modelo: "MODELO IPHONE", imei: "0000000002", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
